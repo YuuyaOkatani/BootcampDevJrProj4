23780d60efd2b176c123a1fac609c2f3a586b7d5
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-06-15 18:38:57 -0300

    closes #3 - funcionalidade do botão somar

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function calc(){
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var output = num1 + num2; 
+    document.getElementById("output").value = output;   
+
+}
+/*
+
+
+function calc(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var output = Number(num1) + Number(num2); 
+    document.getElementById("output").value = output;   
+
+}
+    */
