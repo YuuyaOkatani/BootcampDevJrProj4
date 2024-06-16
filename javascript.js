1302e9df0da8c2d88d3e54ed740b5085c9ad2609
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-06-15 21:20:46 -0300

    closes #4 - 4 operações matemáticas implementadas

--- javascript.js
+++ javascript.js
@@ -1,10 +1,20 @@
-function calc(){
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-    var output = num1 + num2; 
-    document.getElementById("output").value = output;   
+
+function calc(op){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var output = `${num1} ${op} ${num2} `; 
+    document.getElementById("output").value = eval(output);
+
+    /// sem a necessidade de usar o switch nem o if else e nem usar funções distintas
+    /// hehe uma função que faz tudo **risadas maléficas** 
 
 }
+
+
+
+
+
+
 /*
 
 
@@ -13,6 +23,78 @@ function calc(){
     var num2 = document.getElementById("num2").value;
     var output = Number(num1) + Number(num2); 
     document.getElementById("output").value = output;   
+
+}
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+// tentativa 1
+
+function soma(){
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var output = num1 + num2; 
+    document.getElementById("output").value = output;   
+
+}
+
+function sub(){
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var output = num1 - num2; 
+    document.getElementById("output").value = output;
+}
+
+function mult(){
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var output = num1 * num2; 
+    document.getElementById("output").value = output;
+}
+
+function div(){
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var output = num1 / num2; 
+    document.getElementById("output").value = output;
+}
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+// tentativa 2
+
+function calc(op){
+    switch(op) {
+        case '+':
+            var num1 = parseFloat(document.getElementById("num1").value);
+            var num2 = parseFloat(document.getElementById("num2").value);
+            var output = num1 + num2; 
+            document.getElementById("output").value = output;
+        break;
+
+        case '-':
+            var num1 = parseFloat(document.getElementById("num1").value);
+            var num2 = parseFloat(document.getElementById("num2").value);
+            var output = num1 - num2; 
+            document.getElementById("output").value = output;
+        break;
+
+        case '*':
+            var num1 = parseFloat(document.getElementById("num1").value);
+            var num2 = parseFloat(document.getElementById("num2").value);
+            var output = num1 * num2; 
+            document.getElementById("output").value = output;
+        break;
+
+        case '/':
+            var num1 = parseFloat(document.getElementById("num1").value);
+            var num2 = parseFloat(document.getElementById("num2").value);
+            var output = num1 / num2; 
+            document.getElementById("output").value = output;
+        break;
+
+        
+    }
 
 }
     */
